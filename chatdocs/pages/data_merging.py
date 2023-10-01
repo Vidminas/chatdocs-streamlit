8434353112209d187832735f12d127e800f8b002
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2023-10-01 11:55:41 +0100

    Data merging: implement datagrid persistence

--- chatdocs/pages/data_merging.py
+++ chatdocs/pages/data_merging.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from dataclasses import dataclass
 
 import pandas as pd
 import numpy as np
@@ -68,35 +69,71 @@ class Dashboard:
                     if self._dark_mode:
                         mui.IconButton(mui.icon.DarkMode, onClick=self._switch_theme)
                     else:
-                        mui.IconButton(mui.icon.LightMode, sx={"color": "#ffc107"}, onClick=self._switch_theme)
+                        mui.IconButton(
+                            mui.icon.LightMode,
+                            sx={"color": "#ffc107"},
+                            onClick=self._switch_theme,
+                        )
 
         @abstractmethod
         def __call__(self):
             """Show elements."""
             raise NotImplementedError
-        
+
 
 class DataGrid(Dashboard.Item):
     def _handle_edit(self, params):
         print(params)
 
-    def __call__(self, title: str, data: list):
-        with mui.Paper(key=self._key, sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"}, elevation=1):
+    def __call__(
+        self, title: str, data: list, *, checkboxSelection=True, selectionStateKey=None
+    ):
+        with mui.Paper(
+            key=self._key,
+            sx={
+                "display": "flex",
+                "flexDirection": "column",
+                "borderRadius": 3,
+                "overflow": "hidden",
+            },
+            elevation=1,
+        ):
             with self.title_bar(padding="10px 15px 10px 15px", dark_switcher=False):
                 mui.icon.ViewCompact()
                 mui.Typography(title)
 
             with mui.Box(sx={"flex": 1, "minHeight": 0}):
-                self.columns = [{ "field": f"column {column}", "flex": 1 } for column in range(len(data[0]))]
-                self.rows = [{ "id": idx, **{ f"column {column}": value for (column, value) in enumerate(elem) } } for (idx, elem) in enumerate(data)]
+                self.columns = [
+                    {"field": f"column {column}", "flex": 1}
+                    for column in range(len(data[0]))
+                ]
+                self.rows = [
+                    {
+                        "id": idx,
+                        **{
+                            f"column {column}": value
+                            for (column, value) in enumerate(elem)
+                        },
+                    }
+                    for (idx, elem) in enumerate(data)
+                ]
+
+                selection_kwargs = (
+                    {}
+                    if selectionStateKey is None
+                    else {
+                        "selectionModel": st.session_state.get(selectionStateKey, []),
+                        "onSelectionModelChange": sync(selectionStateKey),
+                    }
+                )
 
                 mui.DataGrid(
                     columns=self.columns,
                     rows=self.rows,
-                    checkboxSelection=True,
+                    checkboxSelection=checkboxSelection,
                     disableSelectionOnClick=True,
                     onCellEditCommit=self._handle_edit,
-                    onSelectionModelChange=sync("selected_rows"),
+                    **selection_kwargs,
                 )
 
 
@@ -108,7 +145,7 @@ def reorganise_headers(data: pd.DataFrame):
         source = row.metadatas["source"]
         if not source.endswith(".csv"):
             continue
-        
+
         header = row.documents
         dtype = row.metadatas["dtype"]
         sheets_to_columns[source].append((header, dtype))
@@ -120,7 +157,9 @@ def reorganise_headers(data: pd.DataFrame):
 def suggested_merges(config, threshold: float, data: pd.DataFrame):
     suggestions = []
 
-    for distances in pairwise_distances_chunked(np.vstack(data["embeddings"]), metric="cosine", n_jobs=-1):
+    for distances in pairwise_distances_chunked(
+        np.vstack(data["embeddings"]), metric="cosine", n_jobs=-1
+    ):
         candidate_pairs = np.argwhere(distances < threshold)
 
         for c1_idx, c2_idx in candidate_pairs:
@@ -131,20 +170,36 @@ def suggested_merges(config, threshold: float, data: pd.DataFrame):
             c2 = data.iloc[c2_idx]
             if c1["metadatas"]["source"] == c2["metadatas"]["source"]:
                 continue
-            if not c1["metadatas"]["source"].endswith(".csv") or not c2["metadatas"]["source"].endswith(".csv"):
+            if not c1["metadatas"]["source"].endswith(".csv") or not c2["metadatas"][
+                "source"
+            ].endswith(".csv"):
                 continue
             if c1["metadatas"]["dtype"] != c2["metadatas"]["dtype"]:
                 continue
             suggestions.append((c1, c2))
         break
-    
+
     return suggestions
+
+
+@dataclass
+class CSVHeader:
+    name: str
+    source: str
 
 
 def save_merges(datagrid):
     selected_rows = st.session_state.get("selected_rows", [])
+    merges = []
+
     for row in selected_rows:
-        print(datagrid.rows[row])
+        data = datagrid.rows[row]
+        merges.append((
+            CSVHeader(data["column 0"], data["column 1"]),
+            CSVHeader(data["column 2"], data["column 3"])
+        ))
+
+    st.session_state["merges"] = merges
 
 
 def main():
@@ -163,7 +218,9 @@ def main():
         board1 = Dashboard()
         w1 = SimpleNamespace(
             dashboard=board1,
-            suggested_merges=DataGrid(board1, 0, 0, GRID_COLUMNS, ELEM_HEIGHT, isDraggable=False),
+            suggested_merges=DataGrid(
+                board1, 0, 0, GRID_COLUMNS, ELEM_HEIGHT, isDraggable=False
+            ),
         )
         st.session_state.w1 = w1
 
@@ -171,9 +228,16 @@ def main():
         w2 = SimpleNamespace(
             dashboard=board2,
             **{
-                f"data_grid_{sheet}": DataGrid(board2, (idx * ELEM_WIDTH) % GRID_COLUMNS, (idx * ELEM_HEIGHT) / GRID_COLUMNS, ELEM_WIDTH, ELEM_HEIGHT, minH=4)
+                f"data_grid_{sheet}": DataGrid(
+                    board2,
+                    (idx * ELEM_WIDTH) % GRID_COLUMNS,
+                    (idx * ELEM_HEIGHT) / GRID_COLUMNS,
+                    ELEM_WIDTH,
+                    ELEM_HEIGHT,
+                    minH=4,
+                )
                 for idx, sheet in enumerate(sheets_to_columns)
-            }
+            },
         )
         st.session_state.w2 = w2
 
@@ -183,14 +247,33 @@ def main():
 
     with elements("dashboard"):
         with w1.dashboard(rowHeight=57):
-            threshold = st.sidebar.slider("Header similarity threshold", value=0.02, min_value=0.0, max_value=0.1)
-            w1.suggested_merges("Suggested column merges", [(c1["documents"], c1["metadatas"]["source"], c2["documents"], c2["metadatas"]["source"]) for c1, c2 in suggested_merges(config, threshold, data)])
-            mui.Button("Save merges", isDraggable=False, onClick=lambda: save_merges(w1.suggested_merges))
+            threshold = st.sidebar.slider(
+                "Header similarity threshold", value=0.02, min_value=0.0, max_value=0.1
+            )
+            merge_rows = [
+                (
+                    c1["documents"],
+                    c1["metadatas"]["source"],
+                    c2["documents"],
+                    c2["metadatas"]["source"],
+                )
+                for c1, c2 in suggested_merges(config, threshold, data)
+            ]
+            w1.suggested_merges(
+                "Suggested column merges", merge_rows, selectionStateKey="selected_rows"
+            )
+            mui.Button(
+                "Save merges",
+                isDraggable=False,
+                onClick=lambda: save_merges(w1.suggested_merges),
+            )
 
         with w2.dashboard(rowHeight=57):
             for sheet, sheet_data in sheets_to_columns.items():
-                getattr(w2, f"data_grid_{sheet}")(sheet, sheet_data)
-            
+                getattr(w2, f"data_grid_{sheet}")(
+                    sheet, sheet_data, checkboxSelection=False
+                )
+
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
